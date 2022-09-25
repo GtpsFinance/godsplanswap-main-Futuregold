c401a73668c06d26bb424b722c74f4c063403a28
David Gomadza <112462021+Godsplanswap@users.noreply.github.com>
2022-09-25 11:45:42 +0100

    Update AppBody.tsx

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 
 export const BodyWrapper = styled.div`
   position: relative;
-  max-width: 500px;
-  width: 100%;
-  padding: 0.2rem;
-  border-radius: 1.6rem;
+  max-width: 600px;
+  width: 70%;
+  padding: 0rem;
+  border-radius: 500rem;
   box-shadow: rgba(0, 0, 0, 0.01) 0px 0px 1px, rgba(0, 0, 0, 0.04) 0px 4px 8px, rgba(0, 0, 0, 0.04) 0px 16px 24px,
     rgba(0, 0, 0, 0.01) 0px 24px 32px;
-  background: ${({ theme }) => theme.bg1};
+  background: rgba(13, 11, 177, 0.01);
 
   ${({ theme }) => theme.mediaWidth.upToExtraSmall`
     width: 90%;
